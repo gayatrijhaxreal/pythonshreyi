1dc0ddb28aa88f8fe6a6194331bb02a8602adee1
gayatrijhareal <gayatrixreal@gmail.com>
2025-09-02 21:28:32 +0530

    ques

--- sheriyans/main.py
+++ sheriyans/main.py
@@ -1,4 +1,16 @@
-"""Accept numbers from a user"""
-num = input("Enter a number: ")
-num = int(num)
-print(f"You entered: {num}")
+# """Accept numbers from a user"""
+# num = input("Enter a number: ")
+# num = int(num)
+# print(f"You entered: {num}")
+
+# """ Accept age from the user and print it."""
+# age = input("enter your age:")
+# age= int(age)
+# print(f" your age : {age}")
+
+# """Print(126 > 130)"""
+
+print(126 > 130)
+print((456 == 456) != (235 == 236))
+print(12 < 10 or 45 == 56 or 69 > 70 or 15 != 13)
+print(True and bool(0))
